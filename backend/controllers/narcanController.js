888ea71ceadcd5c9457116c0a58ee6b77c63586d
Anteneh Zewdie <kingzewdie16@gmail.com>
2024-05-21 21:58:59 -0400

    changed narcan requests to send email to preventod  org instead of excel sheet

--- backend/controllers/narcanController.js
+++ backend/controllers/narcanController.js
@@ -1,95 +1,48 @@
 const Narcan = require('../models/narcanModel')
 const mongoose = require('mongoose')
-const xlsx = require('xlsx')
-const fs = require('fs')
-const path = require('path')
+const nodemailer = require('nodemailer')
 
-//Excel file paths 
-const exportDir = path.join(__dirname,'..','exports')
-const excelPath = path.join(exportDir, 'narcan_forms.xlsx')
-const tempExcelPath = path.join(exportDir, 'narcan_forms_temp.xlsx')
+const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth:{
+        user: process.env.USER,
+        pass: process.env.PASS
+    }
+})
 
-if (!fs.existsSync(exportDir)) {
-    fs.mkdirSync(exportDir, { recursive: true });
-}
+const sendEmail = (formData) =>{
+    const mailOptions = {
+        from: process.env.USER,
+        to: 'Kingzewdie16@gmail.com',
+        subject: '**New Narcan Request**' ,
+        text: `New Narcan Request Details:\n\n
+        Organization: ${formData.Organization}\n
+        Address: ${formData.Address}\n
+        Telephone: ${formData.Telephone}\n
+        Number of Overdoses per month: ${formData.Number}\n
+        Availability: ${formData.Availability}\n
+        Requested On: ${formData.RequestedOn}`
 
-const writeExcel = (data, excelPath) =>{
-    let workbook 
-    let worksheet 
-
-    if (fs.existsSync(excelPath)) {
-        workbook = xlsx.readFile(excelPath);
-        worksheet = workbook.Sheets['Narcan Forms'];
-      } else {
-        workbook = xlsx.utils.book_new();
-        worksheet = xlsx.utils.json_to_sheet([]);
-        xlsx.utils.book_append_sheet(workbook, worksheet, 'Narcan Forms');
     }
 
-    xlsx.utils.sheet_add_json(worksheet, data, { skipHeader: true, origin: -1 });
-    xlsx.writeFile(workbook, excelPath);
-}
-
-//handler function for writing  if 
-//to temp file original excel sheet is open 
-
-const handleWriteExcel = (data) => {
-    try {
-        writeExcel(data, excelPath)
-
-    } catch(error){
-        if (error.code === 'EBUSY' || error.code === 'EPERM'){
-            //write to the temporary file if the main excel form is open
-            writeExcel(data,tempExcelPath )
-        }
+    transporter.sendMail(mailOptions,(error, info)=>{
+        if (error){
+            console.error('Error sending email:', error);        }
         else{
-            throw error;
+            console.log('Email sent:', info.response);
         }
-    }
+    })
 }
 
-//function that merges temp excel file with main after main file is closed
-const mergeFiles = ()=>{
-    if (fs.existsSync(tempExcelPath)){
-        
-        const tempWorkbook = xlsx.readFile(tempExcelPath);
-        const tempWorksheet = tempWorkbook.Sheets['Narcan Forms'];
-        const tempData = xlsx.utils.sheet_to_json(tempWorksheet);
-
-        handleWriteExcel(tempData)
-        fs.unlinkSync(tempExcelPath) 
-        //delete temp file after merge
-    }
-
-}
-
-
-
-if (!fs.existsSync(excelPath)){
-    writeExcel([
-        { Organization: 'Organization', 
-        Address: "Address", 
-        Telephone: 'Telephone #', 
-        number: '# of Overdoses seen per month ', 
-        time: 'Availability', 
-        createdAt: 'Requested On' }
-
-    ], excelPath)
-}
-
-mergeFiles()
-
-//get all narcan forms
 const getNarcan = async(req,res)=>{
     const forms = await Narcan.find({}).sort({createdAt: -1})
     res.status(200).json(forms)
 }
 
-//create a new form 
 const createNarcan = async(req,res)=>{
     const {description, address, telephone, number, time} = req.body
 
-   //add to db
+   
    try{
     const form = await Narcan.create({description, address, telephone, number, time})
     //format date
@@ -102,7 +55,7 @@ const createNarcan = async(req,res)=>{
         minute: '2-digit',
       });
 
-    //add to excel sheet 
+     
     const newRequest = {
         Organization: form.description, 
         Address: form.address,
@@ -111,23 +64,15 @@ const createNarcan = async(req,res)=>{
         Availability: form.time, 
         RequestedOn: formCreatedAt + " ET"
     }
-
-    //temp data merges before new data is posted
-    mergeFiles() 
-    handleWriteExcel([newRequest]) 
-
+    sendEmail(newRequest)
     
     res.status(200).json(form)
 
    } catch(error){
-    if (error.code === 'EBUSY') {
-        return res.status(400).json({ error: 'We are currently carrying out requests, please try again in a moment' });
-    }
     res.status(400).json({error:error.message})
    }
 }
 
-//delete a form 
 const deleteNarcan = async(req,res)=>{
     const {id} = req.params
 
