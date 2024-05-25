ed59402274d098cc82e338ec788416ecea1121d2
Anteneh Zewdie <kingzewdie16@gmail.com>
2024-05-24 22:04:38 -0400

    update narcanform

--- frontend/src/components/narcanForm.js
+++ frontend/src/components/narcanForm.js
@@ -19,6 +19,26 @@ const NarcanForm = ()=>{
   return (
     <form className="create" onSubmit={handleSubmit}>
       <h4>Send a Request</h4>
+      <div className="form-info">
+        <p>This is a form to receive bulk 
+        shipments of free Narcan, monthly. 
+        Our Narcan is supplied by state-specific 
+        government agencies. 
+
+        Currently available in  
+        <strong> Allegheny county, PA; 
+        Hillsborough county, FL; 
+        Pinellas county, FL; 
+        Alachua county, FL. 
+        </strong>
+        If not available in your 
+        area, contact us through our website.
+        </p>
+
+      </div>
+
+
+
       <div>
         <label>Organization:</label>
         <br />
