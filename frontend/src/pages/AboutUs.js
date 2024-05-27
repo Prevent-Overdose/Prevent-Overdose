57bfaf79a43e20ac898f1ffc633754cecefa6900
RutviShukla <rutvi.shukla@ufl.edu>
2024-05-27 15:21:16 -0400

    Modifed About US for Mobile View

--- frontend/src/pages/AboutUs.js
+++ frontend/src/pages/AboutUs.js
@@ -11,12 +11,11 @@ const AboutUs = () => {
     return (
         <div className="AboutUs-page">
             <div className="leadership-image-container">
-                <Link to="/founders" style={{ fontSize: '55px'}} className="button-left">FOUNDERS</Link> {/* Update button to Link */}
+                <Link to="/founders" className="button-left">FOUNDERS</Link> {/* Update button to Link */}
                 <img src={LeadershipTitle} alt="Leadership Title" />
-                <Link to="/board" style={{ fontSize: '55px'}} className="button-right">BOARD</Link> {/* Update button to Link */}
+                <Link to="/board" className="button-right">BOARD</Link> {/* Update button to Link */}
             </div>
-            <p style={{ fontSize: '20px', marginTop: '50px'}}>Empowering Change: Student Leaders Committed to the Equitable</p>
-            <p style={{ fontSize: '20px'}}>Distribution of Harm Reduction Service Nationwide</p>
+            <p style={{ fontSize: '20px', marginTop: '50px'}}>Empowering Change Student Leaders Committed to the Equitable <br></br> Distribution of Harm Reduction Service Nationwide</p>
             <div className="apply-image-container">
                 <Link to="/apply">
                     <img src={applyToJoin} alt="apply Image" className="apply-image" />
