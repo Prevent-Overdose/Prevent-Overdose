e1e7f9f92e32adc81278cfa3eaa421b6ec98e761
Rahul Chari <therahulchari@gmail.com>
2024-06-13 00:40:49 -0400

    add mobile swipe capabilities

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState }  from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import {
@@ -6,6 +6,8 @@ import {
     Routes,
     Route,
 } from "react-router-dom";
+import { useSwipeable } from "react-swipeable";
+
 
 import './styles.css'
 
@@ -19,24 +21,38 @@ import FoundersPage from "./pages/FoundersPage"; // Import your FoundersPage com
 import BoardPage from "./pages/BoardPage"; // Import your BoardPage component
 
 function App() {
+  const [isSideNavOpen, setIsSideNavOpen] = useState(false);
+
+  const toggleSideNav = () => {
+    setIsSideNavOpen(!isSideNavOpen);
+  };
+
+  const handlers = useSwipeable({
+    
+    onSwipedRight: () => !isSideNavOpen ? toggleSideNav() : void(0),
+    onSwipedLeft: () => isSideNavOpen ? toggleSideNav() : void(0)
+
+  });
+
   return (
-     <Router>
-            <Navbar />
-            <main>
-                <Routes>
-                    <Route exact path="/" element={<Home />} />
-                    <Route path="/request-narcan" element={<RequestNarcan/>}></Route>
-                    <Route path="/report-overdose" element={<ReportOverdose/>}></Route>
-                    <Route path="/about" element={<AboutUs />} />
-                    {/*<Route path="/resources" element={<Resources />} />*/}
+    <Router>
+      <div {...handlers}>
+        <Navbar isSideNavOpen={isSideNavOpen} toggleSideNav={toggleSideNav}/>
+        <main>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/request-narcan" element={<RequestNarcan/>}></Route>
+            <Route path="/report-overdose" element={<ReportOverdose/>}></Route>
+            <Route path="/about" element={<AboutUs />} />
+            {/*<Route path="/resources" element={<Resources />} />*/}
                     
-                    <Route path="/founders" element={<FoundersPage />} /> {/* Route for Founders page */}
-                    <Route path="/board" element={<BoardPage />} /> {/* Route for Board page */}
-                    
-                </Routes>
-            </main>
-            <Footer />
-        </Router>
+            <Route path="/founders" element={<FoundersPage />} /> {/* Route for Founders page */}
+            <Route path="/board" element={<BoardPage />} /> {/* Route for Board page */}
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
